dc0a3bf8510b417c326561346bcf87ed4165cc49
Eric Sanders <eric.sanders@wellspringworldwide.com>
2014-07-22 11:39:53 -0500

    Calling the twig template

--- web/index.php
+++ web/index.php
@@ -1,9 +1,12 @@
 <?php
 	require_once __DIR__.'/../vendor/autoload.php';
 	$app = new Silex\Application();
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	    'twig.path' => __DIR__ . '/../views',
+	));
 
 	$app->get('/', function () use ($app) {
-	    return 'Hello World';
+	    return $app['twig']->render('index.html.twig');
 	});
 
 	$app->run();
